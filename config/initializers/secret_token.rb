a2bb0a6d97f620b373212e26c720cd7883de417d
famulus <famulus.fusion@gmail.com>
2011-04-30 17:49:22 -0400

    fresh rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btcnearme::Application.config.secret_token = 'ea0811f7fcdd0bc9a4f624281c51d0938857049dd5d804da3ffa88871e28d9b7b7859259f52285494378829ec1b851b21ed3ba92c75c1d57045c9134359bfb7a'
