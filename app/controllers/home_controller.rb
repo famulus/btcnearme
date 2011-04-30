4c43cd2d3899cddaced5f59b65dc43420662a3af
famulus <famulus.fusion@gmail.com>
2011-04-30 19:38:50 -0400

    sanity check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	def index
 		# near = Location.find(:all, :origin =>[37.792,-122.393], :within=>10)
 		# render(text: near)
-		geocode_ip_address
+		# geocode_ip_address
 	end
 	
 end
