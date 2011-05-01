623b80e29b111df79d3ec15ef60030d384341273
famulus <famulus.fusion@gmail.com>
2011-05-01 15:00:55 -0400

    can we locate an ip?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 		# geocode_ip_address
 		
 		Post.within(5, :origin =>[37.792,-122.393])
-@location = IpGeocoder.geocode('12.215.42.19')
+@location = IpGeocoder.geocode(request.remote_ip)
 
 
 		
