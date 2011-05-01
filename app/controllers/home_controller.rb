d097d3459a22d32bfa84752eabf9aee4b1556a74
famulus <famulus.fusion@gmail.com>
2011-05-01 18:23:55 -0400

    save lat and lng for zipcode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,29 @@
 class HomeController < ApplicationController
-	
+
 	def index
 		# near = Location.find(:all, :origin =>[37.792,-122.393], :within=>10)
 		# render(text: near)
 		# geocode_ip_address
-		
+
 		# Post.within(5, :origin =>[37.792,-122.393])
-# @location = IpGeocoder.geocode(request.remote_ip)
+		# @location = IpGeocoder.geocode(request.remote_ip)
 
 
 		@post = Post.new
 		@posts = Post.all
 	end
-	
+
 	def create_post
-				@post = Post.new
+		@post = Post.new
 
 		@post.update_attributes(params[:post])
-		@post.save
-		redirect_to({:controller => :home,:action => :index})
+		@post.lat = MultiGeocoder.geocode(@post.zip_code).lat.to_s rescue nil
+		@post.lng = MultiGeocoder.geocode(@post.zip_code).lng.to_s rescue nil
 		
+		@post.save
+		cookies[:zip_code] = @post.zip_code
+		redirect_to({:controller => :home,:action => :index})
+
 	end
-	
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
 	acts_as_mappable
-	validates_numericality_of :zip_code, :quantity
+	validates_numericality_of :zip_code
 end
