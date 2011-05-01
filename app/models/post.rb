b510b3ed51aa0ef9cb907d3bb3731b54dd41eafb
famulus <famulus.fusion@gmail.com>
2011-05-01 18:48:30 -0400

    set to kilometers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
-	acts_as_mappable
+	acts_as_mappable :default_units => :kms, 
+										:default_formula => :sphere, 
+										:distance_field_name => :distance,
+										:lat_column_name => :lat,
+										:lng_column_name => :lng
+
+
 	validates_numericality_of :zip_code
 end
