679280aedbdc4fb40eba75e2fe7a671bd96ad9e1
famulus <famulus.fusion@gmail.com>
2011-04-30 20:11:44 -0400

    install mysql

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.4.rc1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+# gem 'sqlite3'
 gem 'haml'
 gem 'geokit'
 gem 'mysql'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
 		# near = Location.find(:all, :origin =>[37.792,-122.393], :within=>10)
 		# render(text: near)
 		# geocode_ip_address
+		
+		
+		
 	end
 	
 end
