df2868ca46fd4c3f3001707b0a5b394b1a8be633
famulus <famulus.fusion@gmail.com>
2011-05-01 14:09:37 -0400

    geokit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
+include Geokit
+include Geokit::Geocoders
+include Geokit::IpGeocodeLookup
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	helper :all
+  protect_from_forgery
+  geocode_ip_address
+  
+  before_filter :geokit
+
+  def geokit 
+    store_ip_location
+    @current_location = session[:geo_location]  # @current_location is a GeoLoc instance. 
+  end
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+  
 end
