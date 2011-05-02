94f8f948580960aa7db4f892db5527a5474c984c
famulus <famulus.fusion@gmail.com>
2011-05-01 20:25:31 -0400

    only set zip if it's present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
 
 	def index
-		# near = Location.find(:all, :origin =>[37.792,-122.393], :within=>10)
-		# render(text: near)
-		# geocode_ip_address
-
-		# Post.within(5, :origin =>[37.792,-122.393])
-		# @location = IpGeocoder.geocode(request.remote_ip)
-
-
 		@post = Post.new
 		if cookies[:zip_code].present?
 			@posts = Post.within(20, :origin =>cookies[:zip_code],:order=>'distance')
@@ -17,6 +9,8 @@ class HomeController < ApplicationController
 		end
 	end
 
+
+
 	def create_post
 		@post = Post.new
 
@@ -24,10 +18,13 @@ class HomeController < ApplicationController
 		@post.lat = MultiGeocoder.geocode(@post.zip_code.to_s).lat rescue nil
 		@post.lng = MultiGeocoder.geocode(@post.zip_code.to_s).lng rescue nil	
 
-		@post.save
-		cookies[:zip_code] = @post.zip_code
+		if @post.save
+		cookies[:zip_code] = @post.zip_code if @post.zip_code.present?
 		redirect_to({:controller => :home,:action => :index})
 
 	end
+
+
+
 
 end
