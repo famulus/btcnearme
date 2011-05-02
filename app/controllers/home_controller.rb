178258b961552ef0ed442932704f002058301da3
famulus <famulus.fusion@gmail.com>
2011-05-01 21:49:50 -0400

    Make location country specific

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 		@post = Post.new
 		if cookies[:zip_code].present?
 			if Rails.env == "production"
-				@posts = Post.within(20, :origin =>cookies[:zip_code],:order=>'distance')
+				@posts = Post.within(20, :origin =>"#{cookies[:zip_code]}, #{IpGeocoder.geocode(request.remote_ip).country_code}",:order=>'distance')
 				@posts.sort_by_distance_from(cookies[:zip_code])
 			end
 			 @posts = Post.all if Rails.env == "development"
@@ -17,8 +17,8 @@ class HomeController < ApplicationController
 		@post = Post.new
 
 		@post.update_attributes(params[:post])
-		@post.lat = MultiGeocoder.geocode(@post.zip_code.to_s).lat rescue nil
-		@post.lng = MultiGeocoder.geocode(@post.zip_code.to_s).lng rescue nil	
+		@post.lat = MultiGeocoder.geocode("#{@post.zip_code.to_s}, #{IpGeocoder.geocode(request.remote_ip).country_code}").lat rescue nil
+		@post.lng = MultiGeocoder.geocode("#{@post.zip_code.to_s}, #{IpGeocoder.geocode(request.remote_ip).country_code}").lng rescue nil	
 
 		@post.save
 		cookies[:zip_code] = @post.zip_code if @post.zip_code.present?
