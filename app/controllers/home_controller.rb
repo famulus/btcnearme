5bc5a9b7c24463544870524c96ddbece05c4af46
famulus <famulus.fusion@gmail.com>
2011-05-01 20:37:49 -0400

    flash error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,7 +20,7 @@ class HomeController < ApplicationController
 
 		@post.save
 		cookies[:zip_code] = @post.zip_code if @post.zip_code.present?
-		redirect_to({:controller => :home,:action => :index})
+		redirect_to({:controller => :home,:action => :index}, {:error => flash[:error]})
 
 	end
 
