0f4abbc36e5731b136bb70877dd51fe60bab7e58
famulus <famulus.fusion@gmail.com>
2011-05-14 19:09:03 -0400

    eliminate a call to geo API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
 		@post = Post.new
 		if cookies[:zip_code].present?
 			if Rails.env == "production"
-				location = get_geo_ip(request.remote_ip)
-				@posts = Post.within(60, :origin =>"#{cookies[:zip_code]}, #{location.country_code if location.success}",:order=>'distance') 
+				@ip_location = get_geo_ip(request.remote_ip)
+				@posts = Post.within(60, :origin =>"#{cookies[:zip_code]}, #{@ip_location.country_code if @ip_location.success}",:order=>'distance') 
 				@posts.sort_by_distance_from(cookies[:zip_code]) # order not supported in Rails 3 geokit
 			else
 				@posts = Post.all #Google's API doesn't work locally yet
@@ -18,10 +18,12 @@ class HomeController < ApplicationController
 
 
 	def create_post
+		@ip_location = get_geo_ip(request.remote_ip)
+
 		@post = Post.new
 		@post.update_attributes(params[:post])
-		@post.lat = MultiGeocoder.geocode("#{@post.zip_code.to_s}, #{get_geo_ip.country_code if get_geo_ip.success}").lat rescue nil
-		@post.lng = MultiGeocoder.geocode("#{@post.zip_code.to_s}, #{get_geo_ip.country_code if get_geo_ip.success}").lng rescue nil	
+		@post.lat = MultiGeocoder.geocode("#{@post.zip_code}, #{@ip_location.country_code if @ip_location.success}").lat rescue nil
+		@post.lng = MultiGeocoder.geocode("#{@post.zip_code}, #{@ip_location.country_code if @ip_location.success}").lng rescue nil	
 		@post.save
 		cookies[:zip_code] = @post.zip_code if @post.zip_code.present?
 		cookies[:email] = @post.email if @post.valid?
