1d65a4021610c683822b21b4ebc6a50f8ffb9e3b
famulus <famulus.fusion@gmail.com>
2011-05-14 15:18:56 -0400

    made less important smaller

--- lib/watcher_script.rb
+++ lib/watcher_script.rb
@@ -1,3 +1,3 @@
 # watch( '.' )  {|md| system("open 'http://0.0.0.0:3000'") }
 
-watch( 'app' )  {|md| system("open -g 'http://0.0.0.0:3000'") }
+watch( 'app|public' )  {|md| system("open -g 'http://0.0.0.0:3000'") }
