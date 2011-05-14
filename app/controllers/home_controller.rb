1b17d3f28cb40ad2a5fc833ce303933387576030
famulus <famulus.fusion@gmail.com>
2011-05-14 18:44:09 -0400

    eliminate a call to geo API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ApplicationController
 		@post = Post.new
 		if cookies[:zip_code].present?
 			if Rails.env == "production"
-				@posts = Post.within(60, :origin =>"#{cookies[:zip_code]}, #{get_geo_ip.country_code if get_geo_ip.success}",:order=>'distance') if cookies[:zip_code]
+				location = get_geo_ip
+				@posts = Post.within(60, :origin =>"#{cookies[:zip_code]}, #{location.country_code if location.success}",:order=>'distance') 
 				@posts.sort_by_distance_from(cookies[:zip_code])
 			else
-				@posts = Post.all
+				@posts = Post.all #Google's API doesn't work locally yet
 			end
 			@buying = @posts.select{|p| p.buying_or_selling == "buy"}
 			@selling = @posts.select{|p| p.buying_or_selling == "sell"}
@@ -30,11 +31,8 @@ class HomeController < ApplicationController
 			redirect_to({:controller => :home,:action => :index}, :flash => {:error => @post.errors.full_messages.join(", ")})
 		end
 	end
-	
 
-	def get_geo_ip
-		IpGeocoder.geocode(request.remote_ip)		
-	end
+
 
 
 	def delete_post
@@ -44,5 +42,11 @@ class HomeController < ApplicationController
 		end
 		redirect_to({:controller => :home,:action => :index}, :flash => {:notice => "Email deleted sucessfully"})
 	end 
+
+	private 
+
+	def get_geo_ip
+		IpGeocoder.geocode(request.remote_ip)		
+	end
 
 end
