7d1f574a2080f897b20b6a0b0555b34244630710
famulus <famulus.fusion@gmail.com>
2011-05-20 19:50:54 -0400

    smaller radius, maybe less fail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,12 @@ class HomeController < ApplicationController
 
 	def index
 		@post = Post.new
+
 		if cookies[:zip_code].present?
 			if Rails.env == "production"
 				begin
 					@ip_location = get_geo_ip(request.remote_ip)
-					@posts = Post.within(500, :origin =>"#{cookies[:zip_code]}, #{@ip_location.country_code if @ip_location.success}",:order=>'distance') 
+					@posts = Post.within(300, :origin =>"#{cookies[:zip_code]}, #{@ip_location.country_code if @ip_location.success}",:order=>'distance') 
 					@posts.sort_by_distance_from(cookies[:zip_code]) # order not supported in Rails 3 geokit
 				rescue 
 					@posts = []
