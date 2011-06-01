9be7dc11c35daafb35bc21d6d9a2aa5c2babd7af
famulus <famulus.fusion@gmail.com>
2011-06-01 19:23:13 -0400

    email error message chaneg

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
 
 	
-	validates :email, {:presence => { :message => "^Sweet!     If later you want to post? provide an email." }, :uniqueness => true}
+	validates :email, {:presence => { :message => "^Sweet! &nbsp;&nbsp;&nbsp;     If later you want to post? provide an email." }, :uniqueness => true}
 	validates :zip_code, :presence => true
 	
 	scope :buying, where({buying_or_selling: "buy"})
