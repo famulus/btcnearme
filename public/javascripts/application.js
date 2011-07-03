29ec50af2aa2082dd672d8ed2c8c4771a916b41a
famulus <famulus.fusion@gmail.com>
2011-07-03 16:41:06 -0400

    cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,34 +13,31 @@ function displayError(positionError) {
 
 
 function initialize_map(position) {
-console.log(position.coords.latitude);
 	var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	
+
 	var myOptions = {
 		zoom: 14,
 		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	
+
 	var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-	
+
 }
 
 function initialize(){
 	
 	try {
-  if (typeof(navigator.geolocation) == 'undefined'){
-    gl = google.gears.factory.create('beta.geolocation');
-  } else {
-    gl = navigator.geolocation;
-  }
-} catch(e) {}
+		if (typeof(navigator.geolocation) == 'undefined'){
+			gl = google.gears.factory.create('beta.geolocation');
+		} else {
+			gl = navigator.geolocation;
+		}
+		} catch(e) {}
  
-if (gl) {
-  gl.getCurrentPosition(initialize_map, displayError);
-} else {
-  alert("Geolocation services are not supported by your web browser.");
-}
-
-	
+	if (gl) {
+		gl.getCurrentPosition(initialize_map, displayError);
+	} else {
+		alert("Geolocation services are not supported by your web browser.");
+	}	
 }
